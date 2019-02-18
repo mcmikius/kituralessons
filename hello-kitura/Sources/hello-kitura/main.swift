791d7fca47994f0668da0a601ec1d44a5045ce5a
Michail Bondarenko <mcmikius15@gmail.com>
2019-02-18 15:50:19 +0200

    Reading URL encoded form parameters

--- hello-kitura/Sources/hello-kitura/main.swift
+++ hello-kitura/Sources/hello-kitura/main.swift
@@ -6,6 +6,16 @@ HeliumLogger.use()
 
 let router = Router()
 
+router.post(middleware: BodyParser())
+
+router.post("register") { request, response, next in
+    guard let body = request.body, let values = body.asURLEncoded, let firstName = values["firstName"], let lastName = values["lastName"] else {
+        try response.status(.badRequest).end()
+        return
+    }
+    response.send("First Name = \(firstName) and Last Name = \(lastName)")
+    next()
+}
 router.get("/movies/:genre/year/:year") { request, response,next in
     guard let genre = request.parameters["genre"], let year = request.parameters["year"] else {
         try response.status(.badRequest).end()
@@ -26,7 +36,7 @@ router.get("/movies/:genre") { request, response, next in
     next()
 }
 
-Kitura.addHTTPServer(onPort: 8080, with: router)
+Kitura.addHTTPServer(onPort: 8090, with: router)
 Kitura.run()
 
 
